f1cf56f91b46e6d3bde8b22da62ce6f6ce8e974e
João Santos <josan5368@gmail.com>
2023-12-27 17:41:49 +0100

    test: tests of factory service.

--- tests/test_mimo_factory.py
+++ tests/test_mimo_factory.py
@@ -2,20 +2,30 @@
 
 from unittest import TestCase
 
-from mimo.factory import MimoFactory
 from mimo.message import MessageService
+from mimo.sender import SenderService
+from mimo.factory import MimoFactory
 
 
 class MimoFactoryTestCase(TestCase):
 
+    def setUp(self) -> None:
+        self.host = "http://localhost:9000"
+        self.token = "1234567890"
+        self.factory = MimoFactory(self.host, self.token)
+
     def test_should_create_message_service_from_mimo_factory(self):
         """Should create message service from mimo factory."""
-        host = "http://localhost:9000"
-        token = "1234567890"
 
-        factory = MimoFactory(host, token)
-
-        service = factory.create_message_service()
+        service = self.factory.create_message_service()
 
         self.assertIsNotNone(service)
         self.assertIsInstance(service, MessageService)
+
+    def test_should_create_sender_service_from_mimo_factory(self):
+        """Should creae sender service from mimo factory."""
+
+        service = self.factory.create_sender_service()
+
+        self.assertIsNotNone(service)
+        self.assertIsInstance(service, SenderService)
